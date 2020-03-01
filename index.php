551d722f56dd59c3ee991cef3450e9c0311d4941
otie <otie.prime@gmail.com>
2020-03-01 15:28:54 +0200

    temporary

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 
-
 require_once 'helpers/FileManager.php';
-require_once 'services/FileService.php';
+require_once 'services/DateService.php';
 
-const TOTAL_MONTH = 12;
-const TOTAL_WEEK_DAYS = 7;
+$dates = (new \services\FileService\DateService())->getSalaryDates();
 
-$current_month = date('m');
-$current_year = date('Y');
-
-$d_daysinmonth = date('t', mktime(0,0,0, (int) $current_month,1, (int) $current_year));
-
-for ($day = $d_daysinmonth - TOTAL_MONTH; $day < $d_daysinmonth; $day++) {
-
-}
-
-
-$a = $d_daysinmonth;
-
+$a = $dates;
 ?>
---
+++ services/DateService.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace services\FileService;
+
+/**
+ * Class FileService
+ * @package services\FileService
+ */
+class DateService
+{
+    protected $currentDay;
+    protected $currentDayArray;
+    protected $day;
+    protected $month;
+    protected $year;
+    protected const TOTAL_MONTH = 12;
+    protected const WEEKEND_DAYS = [6, 7];
+    protected const DATE_DAY = 0;
+    protected const DATE_MONTH = 1;
+    protected const DATE_YEAR = 2;
+    protected const DATE_DELIMETER = '-';
+    protected const BONUS_DAY = 15;
+    protected const NEEDED_DAY = 3;
+    protected const TOTAL_WEEK_DAYS = 7;
+
+    public function getDateFormat()
+    {
+        return 'd' . self::DATE_DELIMETER  . 'm'  . self::DATE_DELIMETER . 'Y';
+    }
+
+    public function setCurrentDay()
+    {
+        $this->currentDay = date($this->getDateFormat(), time());
+    }
+
+    protected function setCurrentDayArray()
+    {
+        $this->currentDayArray = explode(self::DATE_DELIMETER, $this->currentDay);
+    }
+
+    protected function setDay()
+    {
+        $this->day = $this->currentDayArray[self::DATE_DAY];
+    }
+
+    protected function setMonth()
+    {
+        $this->month = $this->currentDayArray[self::DATE_MONTH];
+    }
+
+    protected function setYear()
+    {
+        $this->year = $this->currentDayArray[self::DATE_YEAR];
+    }
+
+    protected function getLastDayOfMonth($month, $year)
+    {
+        return date('t', mktime(0,0,0, (int) $month,1, (int) $year));
+    }
+
+    protected function getDate($day, $month, $year)
+    {
+        return $day . self::DATE_DELIMETER . $month . self::DATE_DELIMETER . $year;
+    }
+
+    protected function getFormattedDate($day, $month, $year)
+    {
+        return date($this->getDateFormat(), strtotime($this->getDate($day, $month, $year)));
+    }
+
+    protected function getDayOfWeek($day, $month, $year)
+    {
+        return date('N', strtotime($this->getDate($day, $month, $year)));
+    }
+
+    protected function isWeekend($day, $month, $year)
+    {
+        $dayOfWeek = $this->getDayOfWeek($day, $month, $year);
+        return in_array($dayOfWeek, self::WEEKEND_DAYS);
+    }
+
+    public function init()
+    {
+        $this->setCurrentDay();
+        $this->setCurrentDayArray();
+        $this->setDay();
+        $this->setMonth();
+        $this->setYear();
+    }
+
+    public function getSalaryDate()
+    {
+        $salaryDate = null;
+        $day = $this->getLastDayOfMonth($this->month, $this->year);
+        while ($salaryDate === null) {
+            if ($this->isWeekend($day, $this->month, $this->year)) {
+                $this->day--;
+            } else {
+                $salaryDate = $this->getFormattedDate($day, $this->month, $this->year);
+            }
+        }
+        return $salaryDate;
+    }
+
+    public function getBonusDate()
+    {
+        $bonusDate = null;
+        $bonusDay = self::BONUS_DAY;
+        if ($this->isWeekend($bonusDay, $this->month+1, $this->year)) {
+            $dayOfWeek = $this->getDayOfWeek($bonusDay, $this->month+1, $this->year);
+            $offset = self::TOTAL_WEEK_DAYS - $dayOfWeek + self::NEEDED_DAY;
+            $bonusDay = $bonusDay + $offset;
+        }
+        $bonusDate = $this->getFormattedDate($bonusDay, $this->month+1, $this->year);
+        return $bonusDate;
+    }
+
+    public function getSalaryDates()
+    {
+        $this->init();
+        $dates = [];
+        while ($this->month <= self::TOTAL_MONTH) {
+            $dates[] = [$this->getSalaryDate(), $this->getBonusDate()];
+            $this->month++;
+        }
+        return $dates;
+    }
+}
